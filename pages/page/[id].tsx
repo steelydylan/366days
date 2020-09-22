ff77505730c16c1a28346d89ab99a14862d95ae2
steelydylan <ess_president@me.com>
2020-09-22 19:19:34 +0900

    fix

--- pages/page/[id].tsx
+++ pages/page/[id].tsx
@@ -27,7 +27,11 @@ export default ({
               >
                 <a href={`/${post.data.category}/${post.slug}`}>
                   <LazyBackground
-                    url="https://source.unsplash.com/random"
+                    url={
+                      post.data.images?.length
+                        ? post.data.images[0]
+                        : "https://source.unsplash.com/random"
+                    }
                     className="uc-card-image"
                   />
                   <div className="uc-card-wrap">
